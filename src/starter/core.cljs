dbde245afb1dcc3c9522c5e20dba86015fec8d4a
cookieu <cookieu@gmail.com>
2018-02-08 14:03:36 +0800

    init

---
+++ src/starter/core.cljs
@@ -0,0 +1,20 @@
+(ns starter.core
+  (:require [reagent.core :as r]))
+
+(defonce greeting "hello")
+
+(def who "jinwei")
+
+(defn app []
+  [:div greeting " " who])
+
+(defn stop []
+  (js/console.log "Stopping..."))
+
+(defn start []
+  (js/console.log "Starting...")
+  (r/render [app]
+            (.getElementById js/document "app")))
+
+(defn ^:export init []
+  (start))
